d947d67d587893eedbb1fe0b21fb047851d6f50f
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-05 18:30:20 +0800

    Add logging

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from language_detection import language_detection
+from tweet_processing import process_tweets
+import warnings
+
+warnings.filterwarnings('ignore')
+nltk.download('stopwords')
+print()
 
 language_list = ['en', 'es', 'pt']
 
 # define what each iso-639 code means
-lang_dict = {
+language_dict = {
     'en': 'english',
     'es': 'spanish',
-    'pt': 'portugese'
+    'pt': 'portuguese'
+}
+
+spacy_models = {
+    'en': 'en_core_web_sm',
+    'es': 'es_core_news_sm',
+    'pt': 'pt_core_news_sm'
 }
 
 # define directories
 dataset = './all_annotated.tsv'
 clean_data_dir = './clean_data'
-joblib_dir = './joblib'
 csv_dir = './processed_csv'
 lang = 'en'
 
 language_detection(dataset = dataset, clean_data_dir = clean_data_dir, language_list = language_list)
+
+for language_code in language_list:
+    print("Processing tweets for " + language_dict[language_code])
+    process_tweets(clean_data_dir = clean_data_dir, language_code = language_code, language = language_dict[language_code],spacy_model = spacy_models[language_code], csv_dir = csv_dir)
+
+print('Finish processing all tweets.')
